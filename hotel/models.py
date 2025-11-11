fd8844253000f54fa33ac32a9858f373da870fd4
Anita <anitatam2001@gmail.com>
2025-11-11 21:10:34 +0800

    Step 17 ; booking form

--- hotel/models.py
+++ hotel/models.py
@@ -12,6 +12,10 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'City'
+        verbose_name_plural = 'Cities'  # This fixes the "citys" to "cities"
+
 class Department(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
